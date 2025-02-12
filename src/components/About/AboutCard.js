aa763c3527106700209b4e5fbd833e09099972f5
Ahmad Dimashkie <afd08@mail.aub.edu>
2025-02-12 21:31:33 +0200

    fixed some styling and the about section

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -15,18 +15,21 @@ function AboutCard() {
             <span className="blue"> American University of Beirut.</span>
             <br />
             <br />
-            Apart from coding, I am a very outgoing person who likes to get out
-            of his comfort zone, some other activities that I love to do:
+            Apart from coding, I am a very outgoing person who who enjoys
+            stepping out of my comfort zone, Some of my favorite activities
+            include:
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Travelling, I actually did an exchange semester
-              at the{" "}
+              <ImPointRight /> Travelling, I had the opportunity to complete an
+              exchange semester at the{" "}
               <span className="blue"> University of Toronto, Canada</span>
             </li>
             <li className="about-activity">
-              <ImPointRight /> Giving back to the community, which is why I am
-              volunteering with <span className="blue">UNICEF CLUB</span> at AUB
+              <ImPointRight /> Giving back to the community, both technically
+              and socially. This is why I serve as the{" "}
+              <span className="blue">Webmaster</span> at IEEE AUB and I also am
+              volunteering with <span className="blue">UNICEF CLUB</span> AUB
             </li>
             <li className="about-activity">
               <ImPointRight /> Playing Sports like Padel, Squash and Basketball
